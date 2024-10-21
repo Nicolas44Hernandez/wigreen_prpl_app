3905e9d9879736cbb2da97b9e4edd6ff0ee3c80c
Nicolas44Hernandez <n44hernandezp@gmail.com>
2024-10-21 15:59:50 +0200

    Run app as a service
    * Add service script for run as a service
    * Update recipe

--- files/start.sh
+++ files/start.sh
@@ -2,5 +2,20 @@
 
 export FLASK_APP="server/app:create_app()"
 export FLASK_ENV=PRODUCTION
-cd /usr/srv/
-exec flask run --host '0.0.0.0' --port 6060
+
+case $1 in
+    start)
+        cd /usr/srv/
+        flask run --host '0.0.0.0' --port 6060 &
+        ;;
+    stop)
+        killall -9 python3
+        ;;
+    restart)
+        $0 stop
+        $0 start
+        ;;
+    *)
+        echo "Usage : $0 [start|stop]"
+        ;;
+esac
