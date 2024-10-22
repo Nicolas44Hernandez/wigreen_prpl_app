9215c9026a1572053b466375753859d869f57c22
Nicolas44Hernandez <n44hernandezp@gmail.com>
2024-10-22 16:20:17 +0200

    first pytest

---
+++ files/tests/unit/interface/amx/test_amx_interface.py
@@ -0,0 +1,18 @@
+from server.interfaces.amx_usp_interface import AmxUspInterface
+import os
+
+
+def test_interface_dev():
+    """Test for create interface in development environement"""
+    # GIVEN
+    os.environ['FLASK_ENV'] = "DEVELOPMENT"
+
+    # WHEN
+    interface = AmxUspInterface()
+    interface.read_object(path="test")
+    interface.set_object(path="test", params={"test": "test"})
+    interface.add_object(path="test", params={"test": "test"})
+    interface.del_object(path="test")
+
+    # THEN
+    assert type(interface) == AmxUspInterface
